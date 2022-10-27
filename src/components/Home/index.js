25a74da9881526bee49c8248c4d997075a007ca4
Zurisha <andreamwest7@gmail.com>
2022-10-27 12:57:11 -0500

    added image on Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import hey from '../../assets/images/Hey.jpg'
 
 const Home = (props) => {
 
 
 
 return (
+     <section>
      <div className = "welcome">
         <h2>Welcome!!</h2>
-         <p>I'm Andrea! Have a look around!!</p>
+         <p>I'm Andrea! I'm a Full Stack Web Developer excited to show you what I have done! Have a look around!!</p>
     </div>
+    <div>
+     <img src={hey} alt="hey" className='hey'></img>
+    </div>
+    </section>
 )
 
 }
